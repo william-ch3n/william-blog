4d0bc2f1b0d1b8c67cc674c8441d1f511331e245
William <williamsyoteki@gmail.com>
2022-06-23 00:22:27 +0900

    modified the div layout
    1. added 2 big divs to wrap the main contents and make the position relate to the big div instead of the page itself

--- src/components/MeAndIcons.jsx
+++ src/components/MeAndIcons.jsx
@@ -12,57 +12,109 @@ import { AUTO_TEXT } from '../common/constants/allConstants'
 
 export default function MeAndIcons() {
 
-    // Create reference to store the DOM element containing the animation
-	const el = React.useRef(null);
-    // Create reference to store the Typed instance itself
-    const typed = React.useRef(null);
+
+        // const wrapper = document.getElementById("highlight-effect");
+        const wrapper = useRef(null);
+        const className = "in-view";
+
+        
+
+  // Create reference to store the DOM element containing the animation
+  const el = React.useRef(null);
+  // Create reference to store the Typed instance itself
+  const typed = React.useRef(null);
+
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          wrapper.current.classList.add(className);  
+          return;
+        }
+
+        
+        wrapper.current.classList.remove(className);
+        
+      });
+    },
+    {
+      threshold: 1
+    }
+  );
+
+  React.useEffect(() => {
+    const options = {
+      strings: AUTO_TEXT.STRINGS,
+      typeSpeed: 75,
+      backSpeed: 75,
+      loop: true
+    };
+
+    // elRef refers to the <span> rendered below
+    typed.current = new Typed(el.current, options);
+
+    console.log("check:", wrapper.current);
+
+
+
+    observer.observe(wrapper.current);
+
+
+
+
+
+
+    return () => {
+      // Make sure to destroy Typed instance during cleanup
+      // to prevent memory leaks
+      typed.current.destroy();
+    }
+  }, []);
+
+
+
+
   
-    React.useEffect(() => {
-      const options = {
-          strings: AUTO_TEXT.STRINGS,
-        typeSpeed: 75,
-        backSpeed: 75,
-        loop: true
-      };
-      
-      // elRef refers to the <span> rendered below
-      typed.current = new Typed(el.current, options);
-      
-      return () => {
-        // Make sure to destroy Typed instance during cleanup
-        // to prevent memory leaks
-        typed.current.destroy();
-      }
-    }, [])
 
 
-    return (
-        <Fragment>
 
-            <div>
-                <div>
-                    <span className="pre-auto-text">“</span> 
-                    <br />
-                    <span className="pre-text">I'm </span>
-                    <br />
-                    <span ref={el} className="auto-text"/>
-                    <br />
-                    <span className="post-auto-text">”</span>
-                </div>
+
+  return (
+    <Fragment>
+
+      <div className="meAndIconsDiv">
+      <div>
+        <div>
+          <span className="pre-auto-text">“</span>
+          <br />
+          <section>
+            <div className="highlight-effect" ref={wrapper}>
+              <span className="pre-text">I'm </span>
+              {/* <span class="highlight"> for free!</span> */}
             </div>
+          </section>
+          <br />
+          <span ref={el} className="auto-text" />
+          <br />
+          <span className="post-auto-text">”</span>
+        </div>
+      </div>
 
-            <div>
-                <img className="javaCss" src={java} alt="" />
-                <img className="mysqlCss" src={mysql} alt="" />
-                <img className="reactCss" src={react} alt="" />
-                <img className="githubCss" src={github} alt="" />
-                <img className="dockerCss" src={css3} alt="" />
-            </div>
+      <div>
+        <img className="javaCss" src={java} alt="" />
+        <img className="mysqlCss" src={mysql} alt="" />
+        <img className="reactCss" src={react} alt="" />
+        <img className="githubCss" src={github} alt="" />
+        <img className="css3Css" src={css3} alt="" />
+      </div>
 
-            <div>
-                <img className="me_01" src={me_01} alt="" />
-            </div>
+      <div>
+        <img className="me_01" src={me_01} alt="" />
+      </div>
+    
+    </div>
 
-        </Fragment>
-    )
+    </Fragment>
+  )
 }
