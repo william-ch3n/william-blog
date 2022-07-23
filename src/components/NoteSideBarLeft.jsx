4aa6572f6c7d7af2e0a727a6ea96bf9b608d0c02
William <williamsyoteki@gmail.com>
2022-07-23 18:15:12 +0900

    notes左边栏完成
    1. 完成notes左边栏。（暂时不知道放什么东西，先空着吧。）

---
+++ src/components/NoteSideBarLeft.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import '../static/css/noteSideBarLeft.css'
+
+export default function NoteSideBarLeft() {
+  return (
+    <Fragment>
+      <div className="noteSideBarLeftDiv">
+
+      </div>
+    </Fragment>
+  )
+}
--- src/pages/Notes/Notes.jsx
+++ src/pages/Notes/Notes.jsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react'
+import NoteSideBarLeft from '../../components/NoteSideBarLeft'
 import NoteSideBarRight from '../../components/NoteSideBarRight'
 
 export default function Notes() {
   return (
     <Fragment>
+      <NoteSideBarLeft />
       <NoteSideBarRight />
       
     </Fragment>
